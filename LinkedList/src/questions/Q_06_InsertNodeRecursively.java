83951ca50b12a6dad0a86bc3ae9c45d25dde1d33
Sachin Kumar <sachinyadav1469@gmail.com>
2023-03-23 00:12:33 +0530

    Insert Node Recursively

--- LinkedList/src/questions/Q_06_InsertNodeRecursively.java
+++ LinkedList/src/questions/Q_06_InsertNodeRecursively.java
@@ -4,13 +4,25 @@ import config.MyLinkedList;
 import config.Node;
 
 public class Q_06_InsertNodeRecursively {
-    public Node<Integer> insert(Node<Integer> head,int pos){
+    public static Node<Integer> insert(Node<Integer> head,Integer data,int pos){
+        if(head == null && pos!=0){
+            return null;
+        }
+        if(pos == 0){
+            Node<Integer> newNode = new Node<>(data);
+            newNode.next = head;
+            return newNode;
+        }
+        head.next = insert(head.next,data,pos-1);
         return head;
     }
     public static void main(String[] args) {
         MyLinkedList<Integer> list = new MyLinkedList<>();
         list.add(new Integer[]{1,2,3,5,6,7,8});
         Node<Integer> head = list.getHead();
-        
+        head = insert(head,4,7);
+        list.setHead(head);
+        list.printList();
+        System.out.println("Size of list is: "+list.getSize());
     }
 }
